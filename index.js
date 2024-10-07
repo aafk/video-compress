cd58eb677ae76eff64fbbcc9ce6d1e27ef8c3d43
TalentedDev <webdevstar1997@outlook.com>
2024-10-08 08:41:18 +0900

    version upgrade

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const fs = require('fs');
+const fs = require("fs");
 
-function videoCompress (input,output) {
-    const jsonString = JSON.stringify({
-        "input": input,
-        "output": output
-    })
-    fs.writeFile('./node_modules/video-compressor/src/res/paths.json', jsonString, err => {
-        if (err) {
-            console.log('Error writing file', err)
-        } else {
-            const start = require('./app');
-        }
-    })
+function videoCompress(input, output, size) {
+  const jsonString = JSON.stringify({
+    size,
+    input,
+    output,
+  });
+  fs.access(input, (error) => {
+    if (error) {
+      fs.mkdirSync(input);
+    }
+  });
+  fs.access(output, (error) => {
+    if (error) {
+      fs.mkdirSync(output);
+    }
+  });
+  fs.writeFile(
+    "./node_modules/video-compress/src/res/paths.json",
+    jsonString,
+    (err) => {
+      if (err) {
+        console.log("Error writing file", err);
+      } else {
+        const start = require("./app");
+      }
+    }
+  );
 }
 
-
-module.exports = videoCompress
+module.exports = videoCompress;
